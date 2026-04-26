85246cee21225a3437e3327d2ebfd9cdacee3acb
Aart Albers <aart@sumedia.nl>
2026-04-26 14:05:28 +0200

    Added Vercel speed insights

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: {enabled: true},
-    modules: ['@nuxtjs/tailwindcss'],
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@vercel/speed-insights'
+    ],
     css: ['~/assets/css/main.css'],
     runtimeConfig: {
         public: {
